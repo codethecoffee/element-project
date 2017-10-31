5a4acd4446b2ff3eee00eb23fd43abee0c7e933d
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2017-10-30 23:32:36 -0500

    Apply prettier

--- src/components/elements-list.js
+++ src/components/elements-list.js
@@ -3,35 +3,34 @@ import ElementsListItem from "./elements-list-item";
 import { Grid } from "grid-styled";
 import { Message } from "../styles";
 
-const ElementsList = (props) => {
-
-    if (!props.elements.length) {
-      return (
-        <Message>
-          <center>You haven't created any elements yet!</center>
-        </Message>
-      );
-    }
+const ElementsList = props => {
+  if (!props.elements.length) {
     return (
-      <div>
-        <center>
-          {props.elements.map((element, index) => {
-            return (
-              <Grid p={1}>
-                <center>
-                  <ElementsListItem
-                    key={index}
-                    index={index}
-                    element={element}
-                    deleteElement={props.deleteElement}
-                  />
-                </center>
-              </Grid>
-            );
-          })}
-        </center>
-      </div>
+      <Message>
+        <center>You haven't created any elements yet!</center>
+      </Message>
     );
-}
+  }
+  return (
+    <div>
+      <center>
+        {props.elements.map((element, index) => {
+          return (
+            <Grid p={1}>
+              <center>
+                <ElementsListItem
+                  key={index}
+                  index={index}
+                  element={element}
+                  deleteElement={props.deleteElement}
+                />
+              </center>
+            </Grid>
+          );
+        })}
+      </center>
+    </div>
+  );
+};
 
 export default ElementsList;
